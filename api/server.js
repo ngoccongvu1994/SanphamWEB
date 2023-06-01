20734b2bce4d33f62e4ce9d0dd2f389e4fc8f58a
ngoc cong vu <ngoccongvu1994@gmail.com>
2023-06-01 11:24:51 +0700

    add https  to api

--- api/server.js
+++ api/server.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 const cookieSession = require("cookie-session");
 const dbConfig = require("./app/config/db.config");
 const app = express();
 var corsOptions = {
-  origin: ["http://localhost:4200"],
+  origin: "https://anthanhphu.com.vn",
   credentials: true
 }
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://anthanhphu.com.vn');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+const certificatePath = "/ssl/certs/anthanhphu_com_vn_c5a54_69415_1693267199_b046082d5e7cde4670704063c353df27.crt";
+const privateKeyPath = "/ssl/keys/c5a54_69415_eb03f14517a7ac3062aa3fcb34a1a871";
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const certificate = fs.readFileSync(certificatePath, "utf8");
+
+const options = {
+  key: privateKey,
+  cert: certificate,
+};
 
 app.use(cors(corsOptions));
 
@@ -53,9 +72,12 @@ require("./app/routes/product.routes")(app);
 require("./app/routes/categoryProd.routes")(app);
 require("./app/routes/info.router")(app);
 
+// 
+const server = https.createServer(options, app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
